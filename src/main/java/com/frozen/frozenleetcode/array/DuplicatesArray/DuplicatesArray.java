658b9d47630930dd0575d18b96ae4590e713cf94
jdfrozen <616769416@qq.com>
2019-04-07 19:26:41 +0800

    修改

--- src/main/java/com/frozen/frozenleetcode/array/DuplicatesArray/DuplicatesArray.java
+++ src/main/java/com/frozen/frozenleetcode/array/DuplicatesArray/DuplicatesArray.java
@@ -7,27 +7,37 @@ package com.frozen.frozenleetcode.array.DuplicatesArray;
  */
 public class DuplicatesArray {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DuplicatesArray duplicatesArray = new DuplicatesArray();
-        int[] nums = new int[]{0,1,2,2,3,3,4,5,6,7,7};
-        int[] nums1 = new int[]{};
+        int[] nums = new int[]{0, 1, 2, 2, 3, 3, 4, 5, 6, 7, 7};
+        int[] nums1 = new int[]{1, 1, 2};
         System.out.println(duplicatesArray.removeDuplicates(nums1));
     }
+
     public int removeDuplicates(int[] nums) {
-        if(nums==null||nums.length==0){
+        if (nums == null || nums.length == 0) {
             return 0;
         }
-        int newLength=1;
-        int temp=nums[0];
-        int i =1;
-        while (i<nums.length) {
-            if (nums[i]!= temp) {
+        int[] tempNums = new int[nums.length];
+        int newLength = 0;
+        int temp = nums[newLength];
+        tempNums[newLength] = temp;
+        int i = 1;
+        while (i < nums.length) {
+            if (nums[i] != temp) {
                 temp = nums[i];
                 newLength++;
+                tempNums[newLength] = temp;
             }
             i++;
         }
-        return newLength;
+        int newSize = newLength+1;
+        int[] newNums = new int[newSize];
+        for (i = 0; i <= newLength; i++) {
+            newNums[i] = tempNums[i];
+        }
+        nums = newNums;
+        return newSize;
     }
 
 }
