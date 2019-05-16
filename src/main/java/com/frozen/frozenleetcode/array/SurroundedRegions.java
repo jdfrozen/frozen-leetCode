4a6bce1bb986f6eca47a7ece8b5daac90e1a35ef
jdfrozen <616769416@qq.com>
2019-05-16 10:23:02 +0800

    数组围绕初步编码完成

--- src/main/java/com/frozen/frozenleetcode/array/SurroundedRegions.java
+++ src/main/java/com/frozen/frozenleetcode/array/SurroundedRegions.java
@@ -1,5 +1,7 @@
 package com.frozen.frozenleetcode.array;
 
+import org.springframework.util.StringUtils;
+
 import java.util.*;
 
 /**
@@ -15,9 +17,12 @@ import java.util.*;
 public class SurroundedRegions {
     public static void main(String[] args) {
         char[][] board = new char[][]{
-                {'X', 'X'},
-                {'X', 'O'}
+                {'X', 'X', 'X'},
+                {'X', 'O', 'X'},
+                {'X', 'X', 'O'}
         };
+        SurroundedRegions surroundedRegions =  new SurroundedRegions();
+        surroundedRegions.solve(board);
         for (char[] nums : board) {
             System.out.println(Arrays.toString(nums));
         }
@@ -35,36 +40,113 @@ public class SurroundedRegions {
             return;
         }
         int wide = board[0].length;
-        Map<String, Integer> map = new HashMap<>();
-        for (int i=0;i<=high;i++) {
-            for (int j=0;j<=wide;j++) {
-                char c = board[i][j];
+        //key是坐标位置h&w来表示,表示边界
+        Set<String> boundarySet = new HashSet<>();
+        for (int h = 0; h < high; h++) {
+            for (int w = 0; w < wide; w++) {
+                char c = board[h][w];
                 if ('X' == c) {
                     continue;
                 }
-                List<Character> characters = new ArrayList<>();
-                char upper=;
-                char lower=;
-                char left=;
-                char right=;
+                String key = getKey(h,  w);
+                //如果该点是边界
+                if (h == 0 || w == 0 || h == high - 1 || w == wide - 1) {
+                    boundarySet.add(key);
+                    continue;
+                }
+                //保存这次的遍历值
+                List<String> list = new ArrayList<>();
+                list.add(key);
+                char upper = board[h - 1][w];
+                char lower = board[h + 1][w];
+                char left = board[h][w - 1];
+                char right = board[h][w + 1];
+                boolean upperResult = search(board, upper, "upper", boundarySet, list, h - 1, w, high, wide);
+                boolean lowerResult = search(board, lower, "lower", boundarySet, list, h + 1, w, high, wide);
+                boolean leftResult = search(board, left, "left", boundarySet, list, h, w - 1, high, wide);
+                boolean rightResult = search(board, right, "right", boundarySet, list, h, w + 1, high, wide);
+                //被围住，则进行O->X转换
+                if (upperResult && lowerResult && leftResult && rightResult) {
+                    for (String s : list) {
+                        String[] a = StringUtils.split(s, "&");
+                        board[Integer.valueOf(a[0])][Integer.valueOf(a[1])] = 'X';
+                    }
+                } else {
+                    //没有被围住则记录边界
+                    for (String s : list) {
+                        boundarySet.add(s);
+                    }
+                }
             }
         }
     }
 
-    private Boolean search(char c, String direction, Map<String, Integer> map,int high,int wide) {
+    /**
+     * @param board
+     * @param c
+     * @param direction
+     * @param boundarySet
+     * @param list
+     * @param h
+     * @param w
+     * @param high
+     * @param wide
+     * @return
+     */
+    private Boolean search(char[][] board, char c, String direction, Set<String> boundarySet, List<String> list, int h, int w, int high, int wide) {
         if ('X' == c) {
+            return true;
+        }
+        String key = getKey(h,  w);
+        //如果该点是边界
+        if (h == 0 || w == 0 || h == high - 1 || w == wide - 1) {
+            boundarySet.add(key);
             return false;
         }
+        list.add(key);
+        if (boundarySet.contains(c)) {
+            return false;
+        }
+        char upper = board[h - 1][w];
+        char lower = board[h + 1][w];
+        char left = board[h][w - 1];
+        char right = board[h][w + 1];
+        boolean upperResult = false;
+        boolean lowerResult = false;
+        boolean leftResult = false;
+        boolean rightResult = false;
         switch (direction) {
             case "upper":
+                upperResult = search(board, upper, "upper", boundarySet, list, h - 1, w, high, wide);
+                leftResult = search(board, left, "left", boundarySet, list, h, w - 1, high, wide);
+                rightResult = search(board, right, "right", boundarySet, list, h, w + 1, high, wide);
                 break;
             case "lower":
+                lowerResult = search(board, lower, "lower", boundarySet, list, h + 1, w, high, wide);
+                leftResult = search(board, left, "left", boundarySet, list, h, w - 1, high, wide);
+                rightResult = search(board, right, "right", boundarySet, list, h, w + 1, high, wide);
                 break;
             case "left":
+                upperResult = search(board, upper, "upper", boundarySet, list, h - 1, w, high, wide);
+                lowerResult = search(board, lower, "lower", boundarySet, list, h + 1, w, high, wide);
+                leftResult = search(board, left, "left", boundarySet, list, h, w - 1, high, wide);
                 break;
             case "right":
+                upperResult = search(board, upper, "upper", boundarySet, list, h - 1, w, high, wide);
+                lowerResult = search(board, lower, "lower", boundarySet, list, h + 1, w, high, wide);
+                rightResult = search(board, right, "right", boundarySet, list, h, w + 1, high, wide);
+                break;
+            default:
                 break;
         }
-        return false;
+        if (upperResult && lowerResult && leftResult && rightResult) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String getKey(int h, int w){
+        return String.valueOf(h) + "&" + String.valueOf(w);
     }
 }
