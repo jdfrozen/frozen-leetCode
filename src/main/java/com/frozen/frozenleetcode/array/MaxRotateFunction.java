1e53272369368d35132ac5be70535c81fa364fb3
jdfrozen <616769416@qq.com>
2019-05-20 09:51:58 +0800

    maxRotateFunction

---
+++ src/main/java/com/frozen/frozenleetcode/array/MaxRotateFunction.java
@@ -0,0 +1,21 @@
+package com.frozen.frozenleetcode.array;
+
+/**
+ * @author: Frozen
+ * @create: 2019-05-20 09:50
+ * @description: 给定一个长度为 n 的整数数组 A 。
+ * 假设 Bk 是数组 A 顺时针旋转 k 个位置后的数组，我们定义 A 的“旋转函数” F 为：
+ * F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]。
+ * 计算F(0), F(1), ..., F(n-1)中的最大值。
+ * 注意:
+ * 可以认为 n 的值小于 105。
+ **/
+public class MaxRotateFunction {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxRotateFunction(int[] A) {
+        return 0;
+    }
+}
