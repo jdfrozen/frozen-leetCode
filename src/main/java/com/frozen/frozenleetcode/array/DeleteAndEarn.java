d55e7f86dad0efb5a26328b3c87467e5cce709dd
jdfrozen <616769416@qq.com>
2019-06-21 09:37:02 +0800

    134. Gas Station

--- src/main/java/com/frozen/frozenleetcode/array/DeleteAndEarn.java
+++ src/main/java/com/frozen/frozenleetcode/array/DeleteAndEarn.java
@@ -30,15 +30,15 @@ public class DeleteAndEarn {
             int sum = nums[i];
             int left = nums[i] - 1;
             int right = nums[i] + 1;
-            int leftValue = 0;
+            int leftIndex = 0;
             if (left > 0 && left < length&&left!=i) {
-                leftValue = nums[left];
+                leftIndex = nums[left];
             }
-            int rightValue = 0;
-            if (right > 0 && right < length&&right!=i) {
-                rightValue = nums[right];
+            int rightIndex = 0;
+            if (right > 0 && right < length&&right!=i&&right!=leftIndex) {
+                rightIndex = nums[right];
             }
-            sum = sum + leftValue + rightValue;
+            sum = sum + leftIndex + rightIndex;
             if (sum > result) {
                 result = sum;
             }
