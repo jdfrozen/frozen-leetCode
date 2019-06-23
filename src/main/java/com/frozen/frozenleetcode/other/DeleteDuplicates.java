3717254116d080b18b43d589a39de3f81fc203ae
jdfrozen <616769416@qq.com>
2019-06-23 19:21:04 +0800

    134. Gas Station

--- src/main/java/com/frozen/frozenleetcode/other/DeleteDuplicates.java
+++ src/main/java/com/frozen/frozenleetcode/other/DeleteDuplicates.java
@@ -11,18 +11,33 @@ import java.util.Set;
 public class DeleteDuplicates {
     public static void main(String[] args) {
         DeleteDuplicates duplicates = new DeleteDuplicates();
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(2);
+        head.next=node1;
+        node1.next=node2;
+        node2.next=node3;
+        node3.next=node4;
+        node4.next=node5;
+        duplicates.deleteDuplicates(head);
     }
 
     public ListNode deleteDuplicates(ListNode head) {
+        if(head==null){
+            return null;
+        }
         Set<Integer> set = new HashSet<>();
         ListNode pre=head;
         ListNode temp = head;
         set.add(temp.val);
         while (temp.next!=null){
             temp=temp.next;
-            if(set.contains(temp)) {
+            if(set.contains(temp.val)) {
                 //存在就删除
-                pre.next = temp;
+                pre.next = temp.next;
             }else {
                 set.add(temp.val);
                 pre = pre.next;
