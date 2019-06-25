33375f1b7fb7324c8b1d46692f7a29e27b7112e0
jdfrozen <616769416@qq.com>
2019-06-25 09:28:52 +0800

    443. String Compression

---
+++ src/main/java/com/frozen/frozenleetcode/string/StringCompression.java
@@ -0,0 +1,27 @@
+package com.frozen.frozenleetcode.string;
+
+/**
+ * @author: Frozen
+ * @create: 2019-06-25 09:27
+ * @description: 443. String Compression
+ * Given an array of characters, compress it in-place.
+ * <p>
+ * The length after compression must always be smaller than or equal to the original array.
+ * <p>
+ * Every element of the array should be a character (not int) of length 1.
+ * <p>
+ * After you are done modifying the input array in-place, return the new length of the array.
+ * <p>
+ *  
+ * Follow up:
+ * Could you solve it using only O(1) extra space?
+ **/
+public class StringCompression {
+    public static void main(String[] args) {
+
+    }
+
+    public int compress(char[] chars) {
+        return -1;
+    }
+}
