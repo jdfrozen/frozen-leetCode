ddd0a1a73c57958fa4d144555cb50d86f0debd9b
jdfrozen <616769416@qq.com>
2019-07-01 09:41:23 +0800

    127. Word Ladder

---
+++ src/main/java/com/frozen/frozenleetcode/string/WordLadder.java
@@ -0,0 +1,29 @@
+package com.frozen.frozenleetcode.string;
+
+import java.util.List;
+
+/**
+ * @author: Frozen
+ * @create: 2019-07-01 09:39
+ * @description: 127. Word Ladder
+ * Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+ * <p>
+ * Only one letter can be changed at a time.
+ * Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+ * Note:
+ * <p>
+ * Return 0 if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ * You may assume no duplicates in the word list.
+ * You may assume beginWord and endWord are non-empty and are not the same.
+ **/
+public class WordLadder {
+    public static void main(String[] args) {
+
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return 0;
+    }
+}
