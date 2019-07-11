afeacd054211463de152169fc59652d4b174b1dc
jdfrozen <616769416@qq.com>
2019-07-11 15:18:22 +0800

    127. Word Ladder

---
+++ src/main/java/com/frozen/frozenleetcode/array/MaxEqualRowsAfterFlips.java
@@ -0,0 +1,18 @@
+package com.frozen.frozenleetcode.array;
+
+/**
+ * @author: Frozen
+ * @create: 2019-07-11 15:17
+ * @description:
+ * 给定由若干 0 和 1 组成的矩阵 matrix，从中选出任意数量的列并翻转其上的 每个 单元格。翻转后，单元格的值从 0 变成 1，或者从 1 变为 0 。
+ *
+ * 返回经过一些翻转后，行上所有值都相等的最大行数。
+ *
+ **/
+public class MaxEqualRowsAfterFlips {
+
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        return 0;
+
+    }
+}
