00431378501502b1ec1aee65a2579521121fc53f
jdfrozen <616769416@qq.com>
2019-07-15 09:40:17 +0800

    678. 有效的括号字符串

--- src/main/java/com/frozen/frozenleetcode/string/CheckValidString.java
+++ src/main/java/com/frozen/frozenleetcode/string/CheckValidString.java
@@ -1,15 +1,58 @@
 package com.frozen.frozenleetcode.string;
 
+import java.util.LinkedList;
+
 /**
  * @author: Frozen
  * @create: 2019-07-14 20:09
  * @description: 678. 有效的括号字符串
  **/
 public class CheckValidString {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        CheckValidString checkValidString = new CheckValidString();
+        String s = "";
+        boolean result = checkValidString.checkValidString(s);
+        System.out.println(result);
     }
+
     public boolean checkValidString(String s) {
-        return false;
+        if(s==null){
+            return false;
+        }
+        final char a = '(';
+        final char b = ')';
+        final char cs = '*';
+        LinkedList<Character> listLeft = new LinkedList();
+        LinkedList listWhere = new LinkedList();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (a == c) {
+                listLeft.add(c);
+                continue;
+            }
+            if (a == cs) {
+                listWhere.add(c);
+                continue;
+            }
+            if (a == b) {
+                int lsize = listLeft.size();
+                int wsize = listWhere.size();
+                if (lsize == 0 && listWhere.size() == 0) {
+                    return false;
+                }
+                if (lsize > 0) {
+                    listLeft.removeLast();
+                    continue;
+                }
+                if (wsize > 0) {
+                    listWhere.removeLast();
+                    continue;
+                }
+            }
+        }
+        if(listLeft.size()>listWhere.size()){
+            return false;
+        }
+        return true;
     }
 }
