2904d4da2a024e46fc7b9b341de1bb363cd4f43d
jdfrozen <616769416@qq.com>
2019-07-16 20:15:58 +0800

    678. 有效的括号字符串

---
+++ src/main/java/com/frozen/frozenleetcode/tree/DistanceK.java
@@ -0,0 +1,19 @@
+package com.frozen.frozenleetcode.tree;
+
+import com.frozen.frozenleetcode.tree.utils.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author: Frozen
+ * @create: 2019-07-16 20:15
+ * @description: 863. 二叉树中所有距离为 K 的结点
+ * 给定一个二叉树（具有根结点 root）， 一个目标结点 target ，和一个整数值 K 。
+ * <p>
+ * 返回到目标结点 target 距离为 K 的所有结点的值的列表。 答案可以以任何顺序返回。
+ **/
+public class DistanceK {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        return null;
+    }
+}
