362cdf188c8fe77dbb11d488b6cf061fdd594c1b
jdfrozen <616769416@qq.com>
2019-08-01 16:39:51 +0800

    678. 有效的括号字符串

--- src/main/java/com/frozen/frozenleetcode/string/MinMoves.java
+++ src/main/java/com/frozen/frozenleetcode/string/MinMoves.java
@@ -6,4 +6,14 @@ package com.frozen.frozenleetcode.string;
  * @description: 453. 最小移动次数使数组元素相等
  **/
 public class MinMoves {
+
+    public static void main(String[] args){
+        MinMoves minMoves = new MinMoves();
+        int[] nums = {1,3,4,2,6,2,4};
+        System.out.println(minMoves.minMoves(nums));
+    }
+
+    public int minMoves(int[] nums) {
+        return 0;
+    }
 }
